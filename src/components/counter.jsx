0afb27528fa4fe3e1afcb7df7e556b0369e83718
bthank <bthankachan@yahoo.com>
2021-10-09 14:11:40 -0400

    list processing chgs

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,8 @@ class Counter extends Component {
 
     state = { 
         count: 0,
-        imageUrl: 'https://picsum.photos/200'
+        imageUrl: 'https://picsum.photos/200',
+        tags: ['tag1', 'tag2', 'tag3']
     };
 
     styles = {
@@ -13,15 +14,27 @@ class Counter extends Component {
     };
 
     render() { 
+
         return (
             <React.Fragment>
                 <img src={ this.state.imageUrl} alt="random image" />
                 <br/><br/><br/>
 
-                <span style={ { fontSize: 30 } } className='badge badge-primary m-2'>{ this.formatCount() }</span>
+                <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
                 <button className='btn btn-secondary btn-sm'>Increment</button>
+                <ul>
+                     
+                        { this.state.tags.map(tag => <li key={ tag }>{ tag }</li>)}
+                     
+                </ul>
             </React.Fragment>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
     }
 
     formatCount() {
