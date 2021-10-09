ccca74ce48fb248d7aacf50a5c5d7a043c838239
bthank <bthankachan@yahoo.com>
2021-10-09 09:22:14 -0400

    styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,12 +7,19 @@ class Counter extends Component {
         imageUrl: 'https://picsum.photos/200'
     };
 
+    styles = {
+        fontSize: 50,
+        fontWeight: 'bold'
+    };
+
     render() { 
         return (
             <React.Fragment>
                 <img src={ this.state.imageUrl} alt="random image" />
-                <span>{ this.formatCount() }</span>
-                <button>Increment</button>
+                <br/><br/><br/>
+
+                <span style={ { fontSize: 30 } } className='badge badge-primary m-2'>{ this.formatCount() }</span>
+                <button className='btn btn-secondary btn-sm'>Increment</button>
             </React.Fragment>
         );
     }
