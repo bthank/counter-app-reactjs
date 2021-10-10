f53a20c8910f01fc576267529d7f94aaa7a09bff
bthank <bthankachan@yahoo.com>
2021-10-09 20:24:02 -0400

    misc changes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -8,9 +8,31 @@ class Counter extends Component {
         tags: ['tag1', 'tag2', 'tag3']
     };
 
+//    constructor() {
+//        super();
+//        console.log('In Constructor  this=',this);
+//        this.handleIncrement = this.handleIncrement.bind(this);
+//    }
+
     styles = {
         fontSize: 50,
         fontWeight: 'bold'
+    };
+
+    renderTags() {
+        if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+        return <ul>{ this.state.tags.map(tag => <li key={ tag }>{ tag }</li>)}</ul>;
+    }
+
+    handleIncrement = (product) => {
+        console.log('Increment clicked    this=',this);
+        console.log(product);
+        this.setState({ count: this.state.count + 1});
+    }
+
+    doHandleIncrement = () => {
+        this.handleIncrement({ id: 1});
     };
 
     render() { 
@@ -21,12 +43,9 @@ class Counter extends Component {
                 <br/><br/><br/>
 
                 <span className={ this.getBadgeClasses() }>{ this.formatCount() }</span>
-                <button className='btn btn-secondary btn-sm'>Increment</button>
-                <ul>
-                     
-                        { this.state.tags.map(tag => <li key={ tag }>{ tag }</li>)}
-                     
-                </ul>
+                <button onClick={ () => this.handleIncrement(product) } className='btn btn-secondary btn-sm'>Increment</button>
+                { this.state.tags.length === 0 && 'Please create a new tag!'}
+                { this.renderTags()}
             </React.Fragment>
         );
     }
